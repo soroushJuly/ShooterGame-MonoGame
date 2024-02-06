004055420ae088b3f2dc8f77050d7a1633d02087
soroushJuly <soroushjuly@gmail.com>
2024-02-06 15:48:53 +0000

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShooterGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        //Represents the player  
+        Player player;
+        // Keyboard states used to determine key presses   
+        KeyboardState currentKeyboardState;
+        KeyboardState previousKeyboardState;
+        // Gamepad states used to determine button presses   
+        GamePadState currentGamePadState;
+        GamePadState previousGamePadState;
+        //Mouse states used to track Mouse button press   
+        MouseState currentMouseState;
+        MouseState previousMouseState;
+        // A movement speed for the layer  
+        float playerMoveSpeed;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            // Initialize the player class
+            player = new Player();
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            // Load the player resources   
+            Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+            player.Initialize(Content.Load<Texture2D>("Graphics\\player"), playerPosition);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            // Start drawing  
+            _spriteBatch.Begin();
+            // Draw the Player  
+            player.Draw(_spriteBatch);
+            // Stop drawing  
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShooterGame
+{
+    internal class Player
+    {
+        // Animation representing the player  
+        public Texture2D PlayerTexture;
+        // Position of the Player relative to the upper left side of the screen  
+        public Vector2 Position;
+        // State of the player  
+        public bool Active;
+        // Amount of hit points that player has  
+        public int Health;
+        // Get the width of the player ship  
+        public int Width
+        { get { return PlayerTexture.Width; } }
+        // Get the height of the player ship  
+        public int Height
+        { get { return PlayerTexture.Height; } }
+
+
+        public void Initialize(Texture2D texture, Vector2 position)
+        {
+            PlayerTexture = texture;
+
+            Position = position;
+
+            // Set the player to be active  
+            Active = true;
+            // Set the player health  
+            Health = 100;
+        }
+        public void Update()
+        {
+        }
+        // Probably the same as render()
+        // batches the object to the graphic card
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Vector2 drawPosition;
+            drawPosition.X = Position.X - Width / 2;
+            drawPosition.Y = Position.Y - Height / 2;
+            spriteBatch.Draw(PlayerTexture, drawPosition, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+    }
+}
